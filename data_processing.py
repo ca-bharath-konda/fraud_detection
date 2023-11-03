e85e2a2b284330391f9c82b41e26eb539c801407
ca-bharath-konda <ca.kbharath@gmail.com>
2023-11-03 17:50:34 +0530

    correct sum function and added multiplication function

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,18 @@
 
 print("adding all the data processing code related to fraud detection")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return c
+
+
+print('addition function')
+
+def multiplication(a,b):
+    d = a*b
+    return b
+
+print('multiplication function')
